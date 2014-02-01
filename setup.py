616516f63c4f8846c85ada8cabdb1988180af4a0
davy.wybiral <davy.wybiral@b0b84a82-1934-986c-323c-a74dd5592d25>
2014-02-01 22:51:19 +0000

    Added setup.py file.

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name = 'python-musical',
+    version = '0.1',
+    url = 'https://code.google.com/p/python-musical/',
+    description = 'A python library for dealing with sounds and music',
+    long_description = '',
+    author = 'Wybiral ',
+    author_email = 'davy.wybiral@gmail.com ',
+    requires = [],
+    packages = ['musical', 'musical.audio', 'musical.theory'],
+)
